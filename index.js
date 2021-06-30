85522135806930fe4889c331685819f222a0c99b
Thorsten Panitz <th.panitz@googlemail.com>
2021-06-30 22:16:56 +0200

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log('Test')
 
-const obj = { key1: 'value1', key2: 'value2', key3: 'value3' }
+const obj = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+}
 
 console.log('The object is:', obj)
