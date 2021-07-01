2fab8c5e714f439315027b18dce0acd40e0a034c
Thorsten Panitz <th.panitz@googlemail.com>
2021-07-01 08:52:45 +0200

    Printing test object

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+const obj = {
+  key1: 'Value1',
+  key2: 'value2',
+}
+
+console.log('The object is:', obj)
+
 new Vue({
   render: (h) => h(App),
 }).$mount('#app')
